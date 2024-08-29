9571b9c10198eec9a1a40a3de88f921f9a09b5cc
SamuraiBUPT <d31409163@163.com>
2024-08-29 10:27:49 +0800

    reconstruct the project

--- backend/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-# author:alisen
-# time: 2020/4/28 14:54
-
--- backend/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-# author:alisen
-# time: 2020/5/11 21:58
-
